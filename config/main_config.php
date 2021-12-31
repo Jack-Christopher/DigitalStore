05eca0dae13471e718a66e93905c8c23d6a6b6a7
Jack Christopher <jhuaihuah@unsa.edu.pe>
2021-12-31 00:06:18 -0500

    main config added

---
+++ config/main_config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Valores por defecto que se usan en todo el sistema
+
+define('PROJECT_NAME', 'Digital Store');
+define ('ROOT', dirname(__FILE__, 2));
+// define ('IMAGE_PATH', ROOT."\app\\frontend\public\images");
+// define ('CSS_PATH', ROOT."\app\\frontend\public\css");
